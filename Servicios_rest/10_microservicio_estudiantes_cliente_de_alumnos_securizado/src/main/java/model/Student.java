f2be4d0a211e94300ed1cd458a6800f6360c0471
QuiqueTortosa <49041109+QuiqueTortosa@users.noreply.github.com>
2022-05-06 14:04:09 +0200

    Subiendo ejercicios

---
+++ Servicios_rest/10_microservicio_estudiantes_cliente_de_alumnos_securizado/src/main/java/model/Student.java
@@ -0,0 +1,21 @@
+package model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Student {
+
+	private String nombre;
+	private String cursos;
+	@JsonProperty(value="nota")
+	private double puntuacion;
+	
+}
