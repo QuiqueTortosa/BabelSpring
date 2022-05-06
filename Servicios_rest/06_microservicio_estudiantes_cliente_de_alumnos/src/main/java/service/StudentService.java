8bc8822fa652f5e3f6219bf7e18a8a22e7323f8d
QuiqueTortosa <49041109+QuiqueTortosa@users.noreply.github.com>
2022-05-06 13:00:20 +0200

    Subiendo ejercicio 7 y 8

---
+++ Servicios_rest/06_microservicio_estudiantes_cliente_de_alumnos/src/main/java/service/StudentService.java
@@ -0,0 +1,12 @@
+package service;
+
+import java.util.List;
+
+import model.Student;
+
+public interface StudentService {
+	
+	void altaStudent(Student student);
+	List<Student> studentPorPuntuacionMinima(double puntuacion);
+	
+}
